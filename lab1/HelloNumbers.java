2722d22b9823c33ff5bac41652b9d1dff87b4ef5
TaffyNekumaTV <s334608@studenti.polito.it>
2025-07-03 20:16:09 +0200

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int res = 0;
+        while(x<10){
+            res += x;
+            System.out.print(res+" ");
+            x += 1;
+        }
+    }
+}
